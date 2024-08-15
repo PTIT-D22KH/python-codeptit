0c482c45b9bd2353e7713e137f63ca660fd987db
vctduong <165612382+vctduong@users.noreply.github.com>
2024-08-15 08:05:24 +0000

    add files

---
+++ submit/py01051/py01051.py
@@ -0,0 +1,17 @@
+# py01051.py
+
+def main():
+    # Write your code here
+    for t in range(int(input())):
+        n = input()
+        sumDigits = 0
+        for c in n:
+            sumDigits += int(c)
+        rev = str(sumDigits)[::-1]
+        if (rev == str(sumDigits)):
+            print("YES")
+        else:
+            print("NO")
+
+if __name__ == '__main__':
+    main()
