0f0efc9b52126ff9c5e83f472e9f50386b0d85ad
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-25 16:04:47 +0000

    add files

---
+++ submit/py02018/py02018.py
@@ -0,0 +1,15 @@
+# py02018.py
+
+def main():
+    # Write your code here
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    minn = min(a)
+    maxx = max(a)
+    for i in range(minn, maxx+ 1):
+        if i not in a:
+            print(i)
+            return
+
+if __name__ == '__main__':
+    main()
