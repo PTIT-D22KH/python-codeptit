b63d3916f55849f801163b893b18ec18306e6c53
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-26 01:54:14 +0000

    add files

---
+++ submit/py02036/README.md
@@ -0,0 +1,5 @@
+## py02036
+![alt text](image.png)
+- [py02036.py](py02036.py)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
---
+++ submit/py02036/py02036.py
@@ -0,0 +1,15 @@
+# py02036.py
+
+import math
+def main():
+    # Write your code here
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    a.sort()
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (math.gcd(a[i], a[j]) == 1):
+                print(f"{a[i]} {a[j]}")
+
+if __name__ == '__main__':
+    main()
