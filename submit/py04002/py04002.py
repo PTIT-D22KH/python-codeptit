8fe71db56906b1aceb5a0f15eb833222184b0da0
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-27 02:25:58 +0000

    add files

--- submit/py04002/py04002.py
+++ submit/py04002/py04002.py
@@ -1,18 +1,21 @@
-# py04002.py
 class Rectangle:
-    def __init__(self, height, width, color) -> None:
-        self.height = height
-        self.width = width
-        self.c = color
-    def perimeter(self):
-        return (self.height + self.width) * 2
-    def area(self):
-        return self.height * self.width
-    def color(self):
-        res = self.c[0].upper() + self.c[1:].lower()
-        return res
+    def __init__(self, d, r, c):
+        self.d = d
+        self.r = r
+        self.c = c
 
-if __name__ == '__main__':
-    arr = input().split()
-    r = Rectangle(int(arr[0]), int(arr[1]), (arr[2]))
+    def area(self):
+        return self.d * self.r
+
+    def perimeter(self):
+        return (self.d + self.r) << 1
+
+    def color(self):
+        return self.c[0].upper() + self.c[1:].lower()
+
+arr = input().split()
+if int(arr[0]) > 0 and int(arr[1]) > 0:
+    r = Rectangle(int(arr[0]), int(arr[1]), str(arr[2]))
     print('{} {} {}'.format(r.perimeter(), r.area(), r.color()))
+else:
+    print('INVALID')
