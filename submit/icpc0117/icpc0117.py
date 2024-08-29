a84253ad792f827625cccad59c962a237adeee85
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-08-29 14:32:13 +0000

    add files

---
+++ submit/icpc0117/icpc0117.py
@@ -0,0 +1,12 @@
+# icpc0117.py
+
+def main():
+    # Write your code here
+    a = set()
+    for t in range(int(input())):
+        s = input()
+        a.add(s)
+    print(len(a))
+
+if __name__ == '__main__':
+    main()
