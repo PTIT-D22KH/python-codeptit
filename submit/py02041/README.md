2cab29e8f5497800d548b9f7c259d0c5b55c1482
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-08-29 14:35:01 +0000

    add fies

---
+++ submit/py02041/README.md
@@ -0,0 +1,7 @@
+
+## py02041
+![alt text](image.png)
+- [README.md](README.md)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
+- [py02041.py](py02041.py)
---
+++ submit/py02041/py02041.py
@@ -0,0 +1,10 @@
+# py02041.py
+
+def main():
+    # Write your code here
+    a = int(input())
+    b = int(input())
+    print(a + b)
+
+if __name__ == '__main__':
+    main()
