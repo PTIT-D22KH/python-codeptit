60666a86ad5720c573956966f8184065841e21ad
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-08-30 13:26:34 +0000

    add files

---
+++ submit/pykt037/README.md
@@ -0,0 +1,7 @@
+
+## pykt037
+![alt text](image.png)
+- [README.md](README.md)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
+- [pykt037.py](pykt037.py)
---
+++ submit/pykt037/pykt037.py
@@ -0,0 +1,17 @@
+# pykt037.py
+
+def main():
+    # Write your code here
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWSYZ"
+    for t in range(int(input())):
+        n, b = [int(i) for i in input().split()]
+        res = ""
+        while n > 0:
+            r = n % b
+            res = digits[r] + res
+            n //= b
+        print(res)
+
+
+if __name__ == '__main__':
+    main()
