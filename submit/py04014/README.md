45996c82980d41d532e02ed2228cf1055358f30d
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-04 06:12:46 +0000

    update files

--- submit/py04014/README.md
+++ submit/py04014/README.md
@@ -1,33 +1,9 @@
 
 ## py04014
-Trường THCS XYZ lập bảng điểm tổng kết cho học sinh. Có 10 môn học lần lượt gồm: Toán, Tiếng Việt, Ngoại ngữ, Vật lý, Hóa học, Sinh học, Lịch Sử, Địa, Giáo dục công dân và môn Công nghệ. Trong đó môn Toán và Tiếng Việt tính hệ số 2, các môn còn lại hệ số 1.
-
-Học sinh được xếp hạng theo điểm trung bình:
-
-Từ 9 trở lên: loại XUAT SAC
-Từ 8 đến 8.9: loại GIOI
-Từ 7 đến 7.9: loại KHA
-Từ 5 đến 6.9: loại TB
-Dưới 5: loai YEU
-Hãy lập bảng điểm tổng kết và sắp xếp theo điểm trung bình giảm dần.
-
-Input
-
-Dòng đầu ghi số học sinh (không quá 50).
-
-Thông tin về mỗi học sinh có hai dòng: dòng đầu là họ tên (độ dài không quá 50), dòng thứ 2 gồm 10 số thực trong đoạn [0..10] lần lượt là điểm 10 môn theo đúng thứ tự đã mô tả.
-
-Output
-
-Danh sách đã sắp xếp được ghi ra bao gồm các thông tin:
-
-Mã học sinh (tự động gán tăng dần theo thứ tự nhập, bắt đầu là HS01)
-Họ và tên
-Điểm trung bình (với 1 chữ số phần thập phân)
-Xếp loại
-Trong trường hợp điểm trung bình bằng nhau thì học sinh nào có mã học sinh nhỏ hơn sẽ xếp trên.
 ![alt text](image.png)
+
 - [README.md](README.md)
+- [image.png](image.png)
 - [input.txt](input.txt)
 - [output.txt](output.txt)
 - [py04014.py](py04014.py)
--- submit/py04014/py04014.py
+++ submit/py04014/py04014.py
@@ -1,6 +1,6 @@
-# py04014.py
 class Student:
     count = 0
+
     def __init__(self, name, score_list) -> None:
         self.name = name
         Student.count += 1
@@ -9,9 +9,9 @@ class Student:
         totalScore = score_list[0] * 2 + score_list[1] * 2
         for i in range(2, len(score_list)):
             totalScore += score_list[i]
-        self.avgScore = totalScore / 12.0
+        self.avgScore = round(totalScore / 10 / 1.2, 1)  # Round to 1 decimal place
         result = ""
-        if (self.avgScore >= 9):
+        if self.avgScore >= 9:
             result = "XUAT SAC"
         elif self.avgScore >= 8:
             result = "GIOI"
@@ -22,22 +22,20 @@ class Student:
         else:
             result = "YEU"
         self.result = result
-    
+
     def __str__(self) -> str:
         return f"{self.id} {self.name} {self.avgScore:.1f} {self.result}"
 
 def main():
-    # Write your code here
     n = int(input())
     a = []
     for _ in range(n):
-        name = input()
+        name = input().strip()
         score_list = [float(i) for i in input().split()]
         a.append(Student(name, score_list))
-    a.sort(key = lambda x : (-x.avgScore, x.id))
+    a.sort(key=lambda x: (-x.avgScore, x.id))
     for x in a:
         print(str(x))
-
 
 if __name__ == '__main__':
     main()
