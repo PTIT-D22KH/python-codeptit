19bbf49f2fbb30001916b3da5422ab5e2f890550
duongvu6 <vucongtuanduong6@gmail.com>
2024-09-09 11:34:02 +0000

    updatwe

---
+++ submit/pykt075/README.md
@@ -0,0 +1,4 @@
+## pykt075
+- [pykt075.py](pykt075.py)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
---
+++ submit/pykt075/pykt075.py
@@ -0,0 +1,14 @@
+# pykt075.py
+class Data:
+    def __init__(self, day, name, phoneNumber) -> None:
+        self.day = day
+        self.name = name
+        self.phoneNumber = phoneNumber
+        a = name.strip().split(" ")
+        
+        
+def main():
+    # Write your code here
+
+if __name__ == '__main__':
+    main()
