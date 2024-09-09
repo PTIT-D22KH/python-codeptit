a49a858995a61c0176f299df221e00419ab11e95
duongvu6 <vucongtuanduong6@gmail.com>
2024-09-09 03:34:02 +0000

    add files

---
+++ submit/py04021/README.md
@@ -0,0 +1,4 @@
+## py04021
+- [py04021.py](py04021.py)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
---
+++ submit/py04021/py04021.py
@@ -0,0 +1,13 @@
+# py04021.py
+class Time:
+    def __init__(self) -> None:
+        pass
+class Gamer:
+    count = 0
+    def __init__(self) -> None:
+        pass
+def main():
+    # Write your code here
+
+if __name__ == '__main__':
+    main()
