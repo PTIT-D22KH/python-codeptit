297db1370bf895b4091208dc7839b86cb964500b
duongvu6 <vucongtuanduong6@gmail.com>
2024-09-09 13:48:26 +0000

    update

--- submit/pykt079/README.md
+++ submit/pykt079/README.md
@@ -1,4 +1,7 @@
+
 ## pykt079
-- [pykt079.py](pykt079.py)
+![alt text](image.png)
+- [README.md](README.md)
 - [input.txt](input.txt)
 - [output.txt](output.txt)
+- [pykt079.py](pykt079.py)
--- submit/pykt079/pykt079.py
+++ submit/pykt079/pykt079.py
@@ -2,6 +2,12 @@
 
 def main():
     # Write your code here
+    for _ in range(int(input())):
+        n = int(input())
+        a = [int(i) for i in input().split()]
+        m = min(a)
+        n = max(a)
+        print(n - m + 1 - len(a))
 
 if __name__ == '__main__':
     main()
