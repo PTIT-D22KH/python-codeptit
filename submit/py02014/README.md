46be73abd6be34d0a42f38cbef86411b9339b4a4
duongvu6 <vucongtuanduong6@gmail.com>
2024-09-20 01:08:06 +0000

    add files

---
+++ submit/py02014/README.md
@@ -0,0 +1,4 @@
+## py02014
+- [py02014.py](py02014.py)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
---
+++ submit/py02014/py02014.py
@@ -0,0 +1,8 @@
+# py02014.py
+
+def main():
+    # Write your code here
+
+if __name__ == '__main__':
+    global a
+    main()
