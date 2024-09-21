bf07a1b6bb0fa7c0b3c21e3528d035ab02302ba9
duongvu5 <vucongtuanduong5@gmail.com>
2024-09-21 03:39:40 +0000

    add files

---
+++ submit/py01004/py01004_1.py
@@ -0,0 +1,31 @@
+# py01004.py
+import math
+
+def isPrime(n):
+    if (n < 2):
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if (n % i == 0):
+            return False
+    return True
+def testCase() :
+    n = int(input())
+    count = 0
+    for i in range(1, n):
+        if (math.gcd(i, n) == 1):
+            count += 1
+    if (isPrime(count) == True):
+        print("YES")
+    else:
+        print("NO")
+    
+def main():
+    # Write your code here
+    t = int(input())
+    while t > 0:
+        testCase()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
