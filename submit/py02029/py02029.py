d3cfed4ae7943f517f0d1cf985a61dd902e24d55
duongvu5 <vucongtuanduong5@gmail.com>
2024-09-22 16:49:10 +0000

    add files

--- submit/py02029/py02029.py
+++ submit/py02029/py02029.py
@@ -1,30 +1,18 @@
-def cmp(item):
-    # Sort primarily by frequency (descending), then by value (ascending)
-    return (-item[1], item[0])
+
 
 def main():
-    # Read input
     n, m = [int(i) for i in input().split()]
     a = [int(i) for i in input().split()]
     
-    # Count the frequency of each element
-    d = {}
-    s = 0
+    d = [0] * (m + 1)
     for x in a:
-        if x not in d:
-            d[x] = 1
-        else:
-            d[x] += 1
-        s = max(s, d[x])
-    
-    
-    
-    # Sort the dictionary items by custom key
-    sorted_items = sorted(d.items(), key=cmp)
-    
-    # Print the sorted result
-    for item in sorted_items:
-        print(f"{item[0]}: {item[1]}")
+        d[x]+= 1
+    max_value = max(d)
+    for x in range(1, m + 1):
+        if d[x] == max_value:
+            d[x] = 0
+    res = max(d)
+    print(d.index(res) if res else "NONE")
 
 if __name__ == '__main__':
     main()
