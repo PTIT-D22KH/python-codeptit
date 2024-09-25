9ca6ca335e6460b8200c7858cfa727c0d4591976
duongvu5 <vucongtuanduong5@gmail.com>
2024-09-25 09:09:53 +0000

    add files

---
+++ submit/pykt067/README.md
@@ -0,0 +1,4 @@
+## pykt067
+- [pykt067.py](pykt067.py)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
---
+++ submit/pykt067/pykt067.py
@@ -0,0 +1,7 @@
+# pykt067.py
+
+def main():
+    # Write your code here
+
+if __name__ == '__main__':
+    main()
