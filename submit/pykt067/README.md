071258dc1cd580748cd547a1c124bcb014723cdd
duongvu5 <vucongtuanduong5@gmail.com>
2024-09-25 09:51:20 +0000

    add files

--- submit/pykt067/README.md
+++ submit/pykt067/README.md
@@ -1,4 +1,7 @@
+
 ## pykt067
-- [pykt067.py](pykt067.py)
+![alt text](image.png)
+- [README.md](README.md)
 - [input.txt](input.txt)
 - [output.txt](output.txt)
+- [pykt067.py](pykt067.py)
--- submit/pykt067/pykt067.py
+++ submit/pykt067/pykt067.py
@@ -1,7 +1,32 @@
 # pykt067.py
+def Try(i,n, a, res, used):
+    for j in range(1, n + 1):
+        if not used[j]:
+            a[i] = j
+            # print(a)
+            used[j] = True
+            if i == n - 1:
+                res.append(a[:])
+            else:
+                Try(i + 1, n, a, res, used)
+            used[j] = False
 
+            
 def main():
     # Write your code here
+    for _ in range(int(input())):
+        n = int(input())
+        res = []
+        a = [0] * n
+        used = [False] * (n + 1)
+        Try(0, n, a, res, used)
+        res.sort(reverse = True)
+        print(len(res))
+        for x in res:
+            print(''.join(map(str, x)), end = ' ')
+        print()
+
+
 
 if __name__ == '__main__':
     main()
