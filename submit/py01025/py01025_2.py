f78dbb8468a0a0132f9e6f273af019c4d56b1527
duongvu5 <vucongtuanduong5@gmail.com>
2024-09-28 03:55:36 +0000

    update

--- submit/py01025/py01025_2.py
+++ submit/py01025/py01025_2.py
@@ -21,22 +21,57 @@ def main():
                 t -= 1
             tempDecimalPart += s[t]
             tempDecimalPart = tempDecimalPart[::-1]
-            # print(tempDecimalPart)
-            
+
+            integerPart = ""
             for i in range(t - 1, 0, -1):
                 # print(s[i], end = '')
                 if (s[i].isdigit()):
+                    integerPart += s[i]
+            integerPart += s[0]
+
+            # print(tempDecimalPart)
+            # integerPart = s[:t]
+            # print(integerPart)
+            integerPart = str(int(integerPart))
+            x = ""
+            for i in range(len(integerPart) - 1, 0, -1):
+                if (integerPart[i].isdigit()):
                     count += 1
-                    x += s[i]
+                    x += integerPart[i]
+                    # print(x)
                     if (count == 3):
                         x += ","
                         count = 0
-            x += s[0]
+            x += integerPart[0]
+
+            
+
+
+            # for i in range(t - 1, 0, -1):
+            #     # print(s[i], end = '')
+            #     if (s[i].isdigit()):
+            #         count += 1
+            #         x += s[i]
+            #         if (count == 3):
+            #             x += ","
+            #             count = 0
+            # x += s[0]
+            
             # print(s[t:])
             # x = x + s[t:]
-            res = x[::-1]
+            # print(x)
+            # res = x[::-1]
+            # res = str(int(res))
+            res = x
+            
+            curr = 0
+            while (res[curr] == '0' or not res[curr].isdigit()):
+                curr += 1
+            res = res[curr:]
             res += tempDecimalPart
+            
         else:
+            s = str(int(s))
             for i in range(len(s) - 1, 0, -1):
                 # print(s[i], end = '')
                 if (s[i].isdigit()):
@@ -47,6 +82,10 @@ def main():
                         count = 0
             x += s[0]
             res = x[::-1]
+            curr = 0
+            while (res[curr] == '0' or not res[curr].isdigit()):
+                curr += 1
+            res = res[curr:]
         print(res)
     except :
         raise Exception("So ban nhap vao khong phai so thuc")
