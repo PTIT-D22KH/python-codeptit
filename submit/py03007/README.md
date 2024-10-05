7681527363ac742e83708eeae74e67628001f360
duongvu5 <vucongtuanduong5@gmail.com>
2024-10-05 02:38:02 +0000

    add files

--- submit/py03007/README.md
+++ submit/py03007/README.md
@@ -1,7 +1,10 @@
 
 ## py03007
 ![alt text](image.png)
+
 - [README.md](README.md)
+- [image.png](image.png)
 - [input.txt](input.txt)
 - [output.txt](output.txt)
 - [py03007.py](py03007.py)
+- [py03007_1.py](py03007_1.py)
---
+++ submit/py03007/py03007_1.py
@@ -0,0 +1,24 @@
+# py03007.py
+import re
+def main():
+    # Write your code here
+    s = ""
+    regex = r'[\w\s,:]+'
+    while True:
+        try:
+            s += input()
+        except Exception:
+            break
+    s = re.findall(regex, s)
+    # print(s)
+    for i in s:
+        x = i.strip().lower().split()
+        # print(x)
+        x[0] = x[0].title()
+        # print(" ".join(x))
+
+
+
+
+if __name__ == '__main__':
+    main()
