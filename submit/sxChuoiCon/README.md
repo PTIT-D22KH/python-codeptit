986a7bd95877559b540fd5bb50caa75043017172
duongvu5 <vucongtuanduong5@gmail.com>
2024-10-05 04:10:26 +0000

    add files

---
+++ submit/sxChuoiCon/README.md
@@ -0,0 +1,4 @@
+## sxChuoiCon
+- [sxChuoiCon.py](sxChuoiCon.py)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
---
+++ submit/sxChuoiCon/sxChuoiCon.py
@@ -0,0 +1,10 @@
+# sxChuoiCon.py
+
+def main():
+    # Write your code here
+    s1 = input()
+    s2 = input()
+    
+
+if __name__ == '__main__':
+    main()
