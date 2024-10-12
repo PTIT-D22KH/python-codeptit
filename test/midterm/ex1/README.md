d4bb509233bd83eb250ff910d864ae3e15adb898
duongvu5 <vucongtuanduong5@gmail.com>
2024-10-12 16:05:19 +0000

    add files

---
+++ test/midterm/ex1/README.md
@@ -0,0 +1,2 @@
+![alt text](VuCongTuanDuong_B22DCKH024_Bai1_ketqua.png)
+![alt text](image.png)
--- test/midterm/ex3/temp.py
+++ test/midterm/ex3/temp.py
@@ -1,25 +1,37 @@
-n = int(input("Nhập n: "))
-endU = 1
-endE = 1
-endO = 1
-endA = 1
-endI = 1
-endOthers = 21
-res = 26
+def count_vowel_permutations(N):
+    if N == 0:
+        return 0
 
-for i in range(2, n + 1):
-    newEndU = endO + endOthers
-    newEndE = endA + endOthers
-    newEndO = endOthers
-    newEndA = endE + endU + endOthers
-    newEndI = endE + endO + endOthers
-    newEndOthers = res * 21
-    newRes = newEndU + newEndE + newEndO + newEndA + newEndI + newEndOthers
-    endU = newEndU
-    endE = newEndE
-    endO = newEndO
-    endA = newEndA
-    endI = newEndI
-    endOthers = newEndOthers
-    res = newRes
-print(f"Tong so chuoi co do dai {n} duoc tao nen boi quy luat tren: {res}")
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    transitions = {
+        'a': ['e'],
+        'e': ['a', 'i'],
+        'i': ['a', 'e', 'o', 'u'],
+        'o': ['i', 'u'],
+        'u': ['a']
+    }
+
+    dp = {v: [0] * (N + 1) for v in vowels}
+    #dp[n][v] la so chuoi co chieu dai n ket thuc voi nguyen am v
+
+    # so chuoi co chieu dai 1 la nguyen am thi chi co duy nhat 1
+    for v in vowels:
+        dp[v][1] = 1
+
+
+    for n in range(2, N + 1):
+        for v in vowels:
+            dp[v][n] = 0
+            for prev_v in transitions[v]:
+                dp[v][n] += dp[prev_v][n - 1]
+
+
+    result = 0
+    for v in vowels:
+        result += dp[v][N]
+
+    return result
+
+
+N = int(input())
+print(f"So chuoi duoc tao nen tu luat tren ma co chieu dai {N}: {count_vowel_permutations(N)}")
