20e94ce2bdd6f00201297c5615e28837ae06809a
duongvu6 <vucongtuanduong6@gmail.com>
2024-12-31 03:30:13 +0000

    update

---
+++ submit/pykt068/README.md
@@ -0,0 +1,4 @@
+## pykt068
+- [pykt068.py](pykt068.py)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
---
+++ submit/pykt068/pykt068.py
@@ -0,0 +1,12 @@
+# pykt068.py
+class Subject:
+    def __init__(self, id, name, type):
+        self.id = id
+        self.name = name
+        self.type = type
+    
+def main():
+    # Write your code here
+
+if __name__ == '__main__':
+    main()
